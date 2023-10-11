e5e52d2da03deba16526e127e6bc6b548fb3fa15
nqh4401 <134050393+nqh4401@users.noreply.github.com>
2023-10-11 10:56:44 +0200

    Add files via upload

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,27 +1,40 @@
-//============================================================================
-// Name        : Versuch02Teil1.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+//////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 MMXXII
+// Versuch 02
+//
+// Datei:  main.cpp
+// Inhalt: Hauptprogramm
+//////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
-#include <string>
 
-int main()
+int fibonacci(int n)
 {
-	int f[25];
-	f[0] = 0;
-	f[1] = 1;
-	for(int i = 1; i < 25 ; i++)
-	{
-		f[i+1] = f[i] + f[i-1];
-	}
+    int previousFib = 0;
+    int currentFib = 1;
+    if(n == 0)
+    {
+        return 0;
+    }
+    if(n == 1)
+    {
+        return 1;
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+        int newFib = previousFib + currentFib ;
+        previousFib = currentFib ;
+        currentFib = newFib ;
+    }
+    return currentFib ;
+}
 
-	for(int i = 1; i < 25 ; i++)
-	{
-		std::cout << f[i-1] << std::endl;
-	}
- 	return 0;
+int main ()
+{
+    std::cout << " Fibonacci - Zahlen von 0 bis 12:" << std::endl ;
+    for (int i = 0; i < 12 ; i ++)
+    {
+        std::cout << "f(" << i << ") = " << fibonacci(i) << std::endl ;
+    }
+    return 0;
 }
